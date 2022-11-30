3768acf2c2c55638634cbdfc943bd065895c2a05
Poockel <wilson.lim55555@gmail.com>
2022-12-01 06:12:45 +0800

    message

--- ID-Assg1/script.js
+++ ID-Assg1/script.js
@@ -12,3 +12,7 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".appear");
 hiddenElements.forEach((el) => observer.observe(el))
+
+const toggleNav = () => {
+  document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+}
